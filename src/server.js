5284b2558cc015823eaff3346cb5c774895b7bb5
Dinesh kumar <107987832+medineshkumarm@users.noreply.github.com>
2024-06-18 23:44:54 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 
-const react_url = process.env.FRONTEND_URL;
+// const react_url = process.env.FRONTEND_URL;
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin',react_url );
+  res.setHeader('Access-Control-Allow-Origin',"*" );
   res.setHeader(
     'Access-Control-Allow-Methods',
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
